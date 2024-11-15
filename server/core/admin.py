becf200236da2bf5d7fff375b90c979cfc47a4a1
ChrisKyle <alikhanov.2004@gmail.com>
2024-11-15 17:27:51 +0500

    Chore: Registered Models in Admin

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserProgress)
+admin.site.register(UserBadge)
+admin.site.register(Badge)
+
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Option)
+
+admin.site.register(Quiz)
+admin.site.register(QuizAttempt)
+admin.site.register(QuizQuestionAttempt)
