6f8f83465e01eb99dd0e3f9d04d29f8db3f7524d
ChrisKyle <alikhanov.2004@gmail.com>
2024-11-15 17:01:36 +0500

    Added: User Model

--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    # Link this model to the User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Add your additional fields
+    email = models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return self.user.username
